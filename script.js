9bf17354b47b237756e89af3110a0f882c11ce86
Abhinav Prasai <abhinav.prsai@gmail.com>
2024-09-01 15:01:26 +0100

    sigma sigma

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    if (sidebar.style.width === '250px') {
-        sidebar.style.width = '0';
+function toggleNav() {
+    const fab = document.querySelector('.fab');
+    const navBar = document.querySelector('.nav-bar');
+    
+    fab.classList.toggle('open');
+    navBar.classList.toggle('open');
+    
+    const fabIcon = document.querySelector('.fab-icon');
+    if (fab.classList.contains('open')) {
+        fabIcon.textContent = '-';
     } else {
-        sidebar.style.width = '250px';
+        fabIcon.textContent = '+';
     }
 }
+
+
+document.addEventListener('mousemove', (e) => {
+    const glowElement = document.querySelector('.glow-circle');
+    
+    if (glowElement) {
+        const circleSize = 50; // Size of the glow circle
+        const offsetX = circleSize / 2;
+        const offsetY = circleSize / 2;
+        
+        glowElement.style.transform = `translate(${e.clientX - offsetX}px, ${e.clientY - offsetY}px)`;
+    }
+});
